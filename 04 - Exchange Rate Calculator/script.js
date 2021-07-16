9d8b7027f351ca09aa21471bf00f878572913d04
Nujsvart <kampfar@gmail.com>
2021-07-16 23:44:51 +0300

    Finished

---
+++ 04 - Exchange Rate Calculator/script.js
@@ -0,0 +1,45 @@
+const currencyEl_one = document.querySelector("#currency-one");
+const currencyEl_two = document.querySelector("#currency-two");
+const amountEl_one = document.querySelector("#amount-one");
+const amountEl_two = document.querySelector("#amount-two");
+
+const rateEl = document.querySelector("#rate");
+const swap = document.querySelector("#swap");
+
+// Fetch exchange rates and update the DOM
+
+const calculate = async function () {
+  try {
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+
+    const res = await fetch(
+      `https://open.exchangerate-api.com/v6/latest/${currency_one}`
+    );
+
+    const data = await res.json();
+    const rate = data.rates[currency_two];
+
+    rateEl.textContent = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+    amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Event Listeners
+
+currencyEl_one.addEventListener("change", calculate);
+amountEl_one.addEventListener("input", calculate);
+currencyEl_two.addEventListener("change", calculate);
+amountEl_two.addEventListener("input", calculate);
+
+swap.addEventListener("click", _ => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  calculate();
+});
+
+calculate();
