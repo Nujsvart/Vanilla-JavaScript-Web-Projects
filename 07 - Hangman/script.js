4dcce2ed09beddf13840c4e2a1d39e5d2653fbf4
Nujsvart <kampfar@gmail.com>
2021-09-24 03:04:55 +0300

    starter

---
+++ 07 - Hangman/script.js
@@ -0,0 +1,16 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+const finalMessageRevealWord = document.getElementById('final-message-reveal-word');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+const wordIndex = Math.floor(Math.random() * words.length);
+
+let selectedWord = words[wordIndex];
+
